6ca9beaa836b26bccfef3dc7e4d719a5a0b0660d
char502 <char502@hotmail.com>
2018-02-03 17:34:23 +0000

    hgj

--- main.js
+++ main.js
@@ -1,12 +1,8 @@
 
-var myCat = {
+const myCat = {
     "name": "Meowsalot",
     "species": "cat",
     "favFood": "tuna"
 }
 
-
-const myFavColors = ["blue", "green", "purple"];
-
-myFavColors[0];
-console.log(myFavColors);
+console.log(myCat.name);
