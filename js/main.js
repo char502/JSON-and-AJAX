7c5a774e0e925b49c01c2f0980f52d3aeda39524
char502 <char502@hotmail.com>
2018-02-04 16:17:57 +0000

    hj

--- js/main.js
+++ js/main.js
@@ -1,6 +1,6 @@
 let bttn = document.getElementById("btn");
 
-btn.addEventListener("click", function() {
+bttn.addEventListener("click", function() {
     let ourRequest = new XMLHttpRequest();
 
     ourRequest.open('GET', 'https://learnwebcode.github.io/json-example/animals-1.json');
