313293094cf6cec173a55d899b422e4387eda4e7
char502 <char502@hotmail.com>
2018-02-04 22:13:33 +0000

    Added error handling

--- js/main.js
+++ js/main.js
@@ -8,9 +8,18 @@ btn.addEventListener("click", function() {
     ourRequest.open('GET', 'https://learnwebcode.github.io/json-example/animals-' + pageCounter + '.json');
 
     ourRequest.onload = function () {
-        var ourData = JSON.parse(ourRequest.responseText);
-        renderHTML(ourData);
-        };
+        if (ourRequest.status >= 200 && ourRequest.status < 400) {
+            var ourData = JSON.parse(ourRequest.responseText);
+            renderHTML(ourData);
+        } else {
+            console.log("We connected to the server, but it returned an error");
+        }    
+    };
+    
+    ourRequest.onerror = function() {
+        console.log("Connection error");
+    };
+
     ourRequest.send();
     pageCounter++;
         if (pageCounter > 3) {
@@ -30,8 +39,18 @@ btn.addEventListener("click", function() {
                 } else {
                     htmlString += " and " + data[i].foods.likes[ii];
                 }
-            //
            } 
+
+           htmlString += ' and dislikes ';
+
+            for (var ii = 0; ii < data[i].foods.dislikes.length; ii++) {
+                if (ii == 0) {
+                    htmlString += data[i].foods.dislikes[ii];
+                } else {
+                    htmlString += " and " + data[i].foods.dislikes[ii];
+                }
+            } 
+
            htmlString += '.</p>';
         }
 
