413a59fd1c73d119fa018e16b054004e5d3ff5fb
char502 <char502@hotmail.com>
2018-02-04 20:27:28 +0000

    adding text to dom and page counter for multiple JSON's

--- js/main.js
+++ js/main.js
@@ -1,10 +1,11 @@
+var pageCounter = 1;
 var animalContainer = document.getElementById('animal-info');
 var btn = document.getElementById("btn");
 
 btn.addEventListener("click", function() {
     var ourRequest = new XMLHttpRequest();
 
-    ourRequest.open('GET', 'https://learnwebcode.github.io/json-example/animals-1.json');
+    ourRequest.open('GET', 'https://learnwebcode.github.io/json-example/animals-' + pageCounter + '.json');
 
     ourRequest.onload = function () {
         var ourData = JSON.parse(ourRequest.responseText);
@@ -14,6 +15,12 @@ btn.addEventListener("click", function() {
     });
 
     function renderHTML(data) {
-        var htmlString = "this is a test";
+        var htmlString = "";
+
+        for (var i = 0; i<data.length; i++) {
+            //console.log(data[i]);
+           htmlString += "<p>" + data[i].name + " is a " + data[i].species + "."; 
+        }
+
         animalContainer.insertAdjacentHTML('beforeend', htmlString);   
     }
