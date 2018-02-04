3740d33ad8baaa40fc55fa88e42cbebdccb3a4b6
char502 <char502@hotmail.com>
2018-02-04 17:02:28 +0000

    hj

--- js/main.js
+++ js/main.js
@@ -1,15 +1,18 @@
-
-
-let btn = document.getElementById("btn");
+const animalContainer = document.getElementById(animal-info);
+var btn = document.getElementById("btn");
 
 btn.addEventListener("click", function() {
-    let ourRequest = new XMLHttpRequest();
+    var ourRequest = new XMLHttpRequest();
 
     ourRequest.open('GET', 'https://learnwebcode.github.io/json-example/animals-1.json');
 
     ourRequest.onload = function () {
-        let ourData = JSON.parse(ourRequest.responseText);
-        console.log(ourData[0]);
+        var ourData = JSON.parse(ourRequest.responseText);
+        renderHTML(ourData);
         };
     ourRequest.send();
     });
+
+    function renderHTML(data) {
+        animalContainer.insertAdjacentHTML('beforeend', 'testing 123');   
+    }
